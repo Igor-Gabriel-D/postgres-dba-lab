ca888bb2f344b8b652a855ba88ca252142c07260
Igor-Gabriel-D <igordantas.dev@gmail.com>
2025-12-23 20:31:37 -0300

    head first sql: create table

---
+++ sql/books/head_first_sql/chapter_1/02-create_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE my_contacts (
+    last_name     VARCHAR(30),
+    first_name    VARCHAR(20),
+    email         VARCHAR(50),
+    sex           CHAR(1),
+    birthday      DATE,
+    profession    VARCHAR(50),
+    location      VARCHAR(50),
+    civil_state   VARCHAR(20),
+    interests     VARCHAR(100),
+    looking_for   VARCHAR(100)
+);
+
