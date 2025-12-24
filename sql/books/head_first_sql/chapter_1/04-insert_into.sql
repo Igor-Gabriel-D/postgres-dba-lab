42550ece033a3c52150d09bb710a8171232115ec
Igor-Gabriel-D <igordantas.dev@gmail.com>
2025-12-23 21:37:56 -0300

    Head First SQL: add examples using INSERT INTO

---
+++ sql/books/head_first_sql/chapter_1/04-insert_into.sql
@@ -0,0 +1,7 @@
+INSERT INTO my_contacts
+(last_name, first_name, email, sex, birthday, profession, location, civil_state, interests, looking_for) VALUES
+(
+ 'Anderson', 'Jillian', 'jill_anderson@breakneckpizza.com', 
+ 'F', '05/09/1980', 'Technical Writer', 'Palo Alto, CA', 
+ 'single', 'kayak, reptiles', 'relationships, friends'
+);
