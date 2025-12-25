a7e5c3df7d27a1929c8542067e18ea41b3e4a45f
Igor-Gabriel-D <igordantas.dev@gmail.com>
2025-12-25 09:13:31 -0300

    feat: add NOT NULL constraints

--- sql/books/head_first_sql/chapter_1/02-create_table.sql
+++ sql/books/head_first_sql/chapter_1/02-create_table.sql
@@ -1,13 +1,13 @@
 CREATE TABLE my_contacts (
-    last_name     VARCHAR(30),
-    first_name    VARCHAR(20),
-    email         VARCHAR(50),
-    sex           CHAR(1),
-    birthday      DATE,
-    profession    VARCHAR(50),
-    location      VARCHAR(50),
-    civil_state   VARCHAR(20),
-    interests     VARCHAR(100),
-    looking_for   VARCHAR(100)
+    last_name     VARCHAR(30)  NOT NULL,
+    first_name    VARCHAR(20)  NOT NULL,
+    email         VARCHAR(50)  NOT NULL,
+    sex           CHAR(1)      NOT NULL,
+    birthday      DATE         NOT NULL,
+    profession    VARCHAR(50)  NOT NULL,
+    location      VARCHAR(50)  NOT NULL,
+    civil_state   VARCHAR(20)  NOT NULL,
+    interests     VARCHAR(100) NOT NULL,
+    looking_for   VARCHAR(100) NOT NULL
 );
 
