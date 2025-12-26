a10c92cfd974902b0ddd543ea0a46d0cd2397f69
Igor-Gabriel-D <igordantas.dev@gmail.com>
2025-12-26 15:55:03 -0300

    feat(db): add easy drinks queries

---
+++ sql/books/head_first_sql/chapter_2/09-easy_drinks_queries.sql
@@ -0,0 +1,21 @@
+SELECT * FROM easy_drinks WHERE main = 'Sprite';
+
+SELECT * FROM easy_drinks WHERE main = 'soda';
+
+SELECT * FROM easy_drinks WHERE quantity2 = 6;
+
+SELECT * FROM easy_drinks WHERE secundary = 'orange juice';
+
+SELECT * FROM easy_drinks WHERE quantity1 = 1.5;
+
+SELECT * FROM easy_drinks WHERE quantity2 < 1;
+
+SELECT * FROM easy_drinks WHERE main = 'soda';
+
+SELECT * FROM easy_drinks WHERE quantity1 < 1.5;
+
+SELECT drink_name, main, secundary FROM easy_drinks WHERE main = 'soda';
+
+
+
+
