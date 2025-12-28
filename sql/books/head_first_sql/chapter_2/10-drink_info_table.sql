8378d5fff565b91ec380d35b0601276a94dbd71d
Igor-Gabriel-D <igordantas.dev@gmail.com>
2025-12-28 16:01:51 -0300

    feat(db): Add SQL inserts for info_drinks

--- sql/books/head_first_sql/chapter_2/10-drink_info_table.sql
+++ sql/books/head_first_sql/chapter_2/10-drink_info_table.sql
@@ -1,4 +1,9 @@
 CREATE TABLE drink_info
 (
-    
+    drink_name    VARCHAR(30),
+    price         DEC(3,2),
+    carbohydrates DEC(3,1),
+    color         VARCHAR(20),
+    ice           VARCHAR(1),
+    calories      INT
 );
---
+++ sql/books/head_first_sql/chapter_2/11-drink_info_insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO drink_info VALUES
+('Blackthorn', 3, 8.4, 'yellow', 'Y', 33),
+('Blue Moon', 2.5, 3.2, 'blue', 'Y', 12),
+('Oh My Gosh', 3.5, 8.6, 'orange', 'Y', 35),
+('Lime Fizz', 2.5, 5.4, 'green', 'Y', 24),
+('Kiss on the Lips', 5.5, 42.5, 'purple', 'Y', 171),
+('Hot Gold', 3.2, 32.1, 'orange', 'N', 195),
+('Lone Tree', 3.6, 4.2, 'red', 'Y', 17),
+('Greyhound', 4, 14, 'yellow', 'Y', 50),
+('Indian Summer', 2.8, 7.2, 'brown', 'N', 30),
+('Bull Frog', 2.6, 21.5, 'cinnamon', 'Y', 80),
+('Soda and it', 3.8, 4.7, 'red', 'N', 19);
+
