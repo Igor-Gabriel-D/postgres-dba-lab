8e0c728fa242255c4ecf8dcdc76537ccf2f42f37
Igor-Gabriel-D <igordantas.dev@gmail.com>
2025-12-28 16:38:47 -0300

    feat(db): Add SQL queries for info_drinks

---
+++ sql/books/head_first_sql/chapter_2/12-drink_info_queries.sql
@@ -0,0 +1,11 @@
+SELECT drink_name FROM drink_info WHERE price >= 3.5 AND calories < 50;
+
+SELECT price FROM drink_info WHERE ice = 'Y' AND color = 'yellow' AND calories > 33;
+
+SELECT drink_name, color FROM drink_info WHERE carbohydrates <= 4 and ice = 'Y';
+
+SELECT price FROM drink_info WHERE calories >= 80;
+
+SELECT drink_name, color, ice FROM drink_info WHERE drink_name = 'Greyhound' OR drink_name = 'Kiss on the Lips';
+
+SELECT drink_name FROM drink_info WHERE drink_name >= 'L' AND drink_name < 'M';
