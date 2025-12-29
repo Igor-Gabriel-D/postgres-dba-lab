e7cace4e24a09f77308c51cdc969a57abacf0932
Igor-Gabriel-D <igordantas.dev@gmail.com>
2025-12-29 20:18:35 -0300

    feat: add query examples demonstrating LIKE patterns

--- sql/books/head_first_sql/chapter_2/12-drink_info_queries.sql
+++ sql/books/head_first_sql/chapter_2/12-drink_info_queries.sql
@@ -9,3 +9,9 @@ SELECT price FROM drink_info WHERE calories >= 80;
 SELECT drink_name, color, ice FROM drink_info WHERE drink_name = 'Greyhound' OR drink_name = 'Kiss on the Lips';
 
 SELECT drink_name FROM drink_info WHERE drink_name >= 'L' AND drink_name < 'M';
+
+SELECT drink_name FROM drink_info WHERE calories IS NULL;
+
+SELECT drink_name FROM drink_info WHERE drink_name LIKE 'B%';
+
+SELECT drink_name, color FROM drink_info WHERE color LIKE '____n';
