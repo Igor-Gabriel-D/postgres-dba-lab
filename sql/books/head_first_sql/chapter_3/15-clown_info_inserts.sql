6e4b103c67407ea88579bbcc99f4b195b6e0e045
Igor-Gabriel-D <igordantas.dev@gmail.com>
2026-01-02 12:41:26 -0300

    feat: add clown_info inserts

---
+++ sql/books/head_first_sql/chapter_3/15-clown_info_inserts.sql
@@ -0,0 +1,11 @@
+INSERT INTO clown_info(name, last_seen, appearance, activities) VALUES
+('Elsie', 'Cherry Hill Senior Center', 'F, red hair, green dress, huge feet', 'balloons, cart'),
+('Pickles', 'Jack Green''s Party', 'M, orange hair, blue suit, huge feet', 'mime'),
+('Snuggles', 'Ball Market', 'F, yellow shirt, long red pants', 'horn, umbrella'),
+('Mr. Hobo', 'BG Circus', 'M, cigar, black hair, tall hat', 'violin'),
+('Clarabelle', 'Belmont Senior Center', 'F, pink hair, giant flowers, blue dress', 'shouting, dancing'),
+('Scooter', 'Oakland Hospital', 'M, blue hair, red suit, huge nose', 'balloons'),
+('Zippo', 'Millstone Shopping Center', 'F, orange suit, long pants', 'dancing'),
+('Babe', 'Earl''s Auto Parts', 'F, all pink and shiny', 'juggling, cart'),
+('Bonzo', NULL, 'M, long polka-dot outfit', 'singing, dancing'),
+('Sniffles', 'Tracy''s', 'M, green and purple suit, pointy nose', NULL);
